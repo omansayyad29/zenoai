f44aa70d9c58ce24cddc0b3c678a9596a6070675
Oman Sayyad <omansayyad29@gmail.com>
2025-07-22 18:29:10 +0530

    backend done

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,7 +1,10 @@
 import OpenAI from "openai";
 import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
-
+import { v2 as cloudinary } from "cloudinary";
+import axios from "axios";
+import fs from "fs";
+import pdf from "pdf-parse/lib/pdf-parse.js";
 const AI = new OpenAI({
   apiKey: process.env.GEMINI_API_KEY,
   baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
@@ -104,19 +107,123 @@ export const generateImage = async (req, res) => {
 
     const formData = new FormData();
     formData.append("prompt", prompt);
-    await axios.post("https://clipdrop-api.co/text-to-image/v1");
+    const data = await axios.post(
+      "https://clipdrop-api.co/text-to-image/v1",
+      formData,
+      {
+        headers: { "x-api-key": process.env.CLIPDROP_API_KEY },
+        responseType: "arraybuffer",
+      }
+    );
 
-    await sql`INSERT INTO creations (user_id, content, prompt,type) VALUES (${userId}, ${content}, ${prompt}, 'blog-title')`;
-    if (plan !== "premium") {
-      await clerkClient.users.updateUserMetadata(userId, {
-        privateMetadata: {
-          free_usage: free_usage + 1,
-        },
-      });
-    }
-    res.json({ success: true, content });
+    const base64Image = `data:image/png;base64,${Buffer.from(
+      data.data,
+      "binary"
+    ).toString("base64")}`;
+    const { secure_url } = await cloudinary.uploader.upload(base64Image);
+
+    await sql`INSERT INTO creations (user_id, content, prompt,type,publish) VALUES (${userId}, ${secure_url}, ${prompt}, 'image',${
+      publish ?? false
+    })`;
+
+    res.json({ success: true, content: secure_url });
   } catch (error) {
     console.log(error.message);
-    res.js;
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const removeImageBackground = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { image } = req.file;
+    const plan = req.plan;
+    if (plan !== "premium") {
+      return res.json({
+        success: false,
+        message: "This feature is only available to premium users",
+      });
+    }
+    const { secure_url } = await cloudinary.uploader.upload(image.path, {
+      transformation: [
+        { effect: "background_removal", background_removal: "" },
+      ],
+    });
+    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId},'Remove Background From Image' ,${secure_url}, 'image')`;
+    res.json({ success: true, content: secure_url });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const removeImageObject = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { image } = req.file;
+    const plan = req.plan;
+    const { object } = req.body();
+    if (plan !== "premium") {
+      return res.json({
+        success: false,
+        message: "This feature is only available to premium users",
+      });
+    }
+    const { public_id } = await cloudinary.uploader.upload(image.path);
+
+    const imageUrl = cloudinary.url(public_id, {
+      transformation: [{ effect: `gen_remove:${object}` }],
+      resource_type: "image",
+    });
+    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId},${`Remove ${object} From Image`} ,${imageUrl}, 'image')`;
+    res.json({ success: true, content: imageUrl });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const resumeReview = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const resume = req.file;
+    const plan = req.plan;
+
+    if (plan !== "premium") {
+      return res.json({
+        success: false,
+        message: "This feature is only available to premium users",
+      });
+    }
+
+    if (resume.size > 5 * 1024 * 1024) {
+      return res.json({
+        success: false,
+        message: "File size should be less than 5MB",
+      });
+    }
+    const dataBuffer = fs.readFileSync(resume.path);
+    const pdfData = await pdf(dataBuffer);
+
+    const prompt = `Review the following resume and provide constructive feedback on its strength,weaknesses, and areas for improvement. Resume Content:\n\n${pdfData.text}`;
+
+    const response = await AI.chat.completions.create({
+      model: "gemini-2.0-flash",
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 0.7,
+      max_tokens: 1000,
+    });
+
+    const content = response.choices[0].message.content;
+    await sql`INSERT INTO creations (user_id, prompt, content, type) VALUES (${userId},'Resume Review' ,${content}, 'resume-review')`;
+    res.json({ success: true, content: content });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
   }
 };
--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ import cors from "cors";
 import "dotenv/config";
 import { clerkMiddleware, requireAuth } from "@clerk/express";
 import aiRouter from "./routes/aiRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import userRouter from "./routes/userRoutes.js";
 const app = express();
+await connectCloudinary();
 app.use(cors());
 app.use(express.json());
 app.use(clerkMiddleware());
@@ -12,6 +15,8 @@ app.get("/", (req, res) => {
 });
 app.use(requireAuth());
 app.use("/api/ai", aiRouter);
-app.listen(5000, () => {
+app.use("/api/user", userRouter);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log("server is running on port 5000");
 });
